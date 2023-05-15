cbed2ff82c31f989a21468535f21c51481d30b72
SalKikal <salomekikalishvili2000@gmail.com>
2023-05-15 23:46:07 +0400

    15.05

--- src/app/image-video/image-video.component.ts
+++ src/app/image-video/image-video.component.ts
@@ -13,8 +13,19 @@ export class ImageVideoComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  Images: string[] = ['assets/Images/process1.png', 'assets/Images/process3.png', 
-                      'assets/Images/process4.png', 'assets/Images/process5.png'];
+  Images: string[] = ['assets/Images/1.png',  'assets/Images/4.png',
+                      'assets/Images/5.png', 
+                      'assets/Images/7.png', 'assets/Images/8.png',
+                      'assets/Images/9.png','assets/Images/2.png', 
+                      'assets/Images/6.png', 'assets/Images/3.png',
+                      'assets/Images/13.png', 'assets/Images/18.png', 
+                      'assets/Images/100.png', 'assets/Images/14.png', 
+                      'assets/Images/15.png', 'assets/Images/16.png',
+                      'assets/Images/12.png','assets/Images/23.png', 
+                      'assets/Images/19.png', 'assets/Images/20.png',
+                      'assets/Images/21.png', 'assets/Images/22.png', 
+                      'assets/Images/process1.png', 'assets/Images/process3.png', 
+                      'assets/Images/process4.png', ];
 
   ImageClicked: string = '';
   openImage(image: string){
